4fbfad8156226337d35f54080f2f06e9667a7d9e
qara70 <entsuka70@gmail.com>
2022-08-30 16:13:09 +0900

    add: GraphQLModuleのimport

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      // schemaファイルパスしてい
+      autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
+      // 生成されるschemaを自動でソートするオプションの有効化
+      sortSchema: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
