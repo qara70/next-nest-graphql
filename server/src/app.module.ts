1c35d077afae4c68a13ec03805dbb25384ff48be
qara70 <entsuka70@gmail.com>
2022-08-31 06:42:36 +0900

    fix & update: @nestjs/graphql v10移行に伴うGraphQL Moduleインポート変更対応・pathモジュールインポート修正

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,15 +1,18 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import path from 'path';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      // schemaファイルパスしてい
-      autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      // @nestjs/graphql v10でApolloかMercuriusを選択できるような変更が入っている
+      driver: ApolloDriver,
+      // schemaファイルパス指定
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       // 生成されるschemaを自動でソートするオプションの有効化
       sortSchema: true,
     }),
